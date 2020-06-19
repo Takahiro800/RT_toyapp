6ba24e0a766272fd910d81c8503fd0cd3acffbc6
Takahiro Abe <hirotaka081630@gmail.com>
2020-06-20 07:20:27 +0900

    nemeカラム削除のDB反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200619220943) do
+ActiveRecord::Schema.define(version: 20200619221832) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20200619220943) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "neme"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
